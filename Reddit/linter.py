8ea40fb5f312a75148741d78edcd7af3381c7a86
EZ <zaireric@gmail.com>
2019-06-25 23:18:40 -0400

    Added comment as to how to run linter.py through docker

--- Reddit/linter.py
+++ Reddit/linter.py
@@ -1,2 +1,10 @@
+'''
+Author:	Eric Zair
+File:	linter.py
+Decription:	Very simple script that is used to check all litner for every python file in
+			this project.
+			To run with docker, run the following command:
+				docker-compose run app python3 linter.py
+'''
 import subprocess
 subprocess.call('pylint_runner')
