df280dfecad677467feffd483c84ff32bca98e57
EZ <zaireric@gmail.com>
2020-04-16 09:35:33 -0400

    Updated dev_seup to make sure that nltk can be installed correctly

--- Reddit/dev_setup.sh
+++ Reddit/dev_setup.sh
@@ -1,1 +1,8 @@
+# Make sure that python is setup correctly.
+sudo apt-get install python3 python-dev python3-dev \
+     build-essential libssl-dev libffi-dev \
+     libxml2-dev libxslt1-dev zlib1g-dev \
+     python-pip -y
+
+# Install all required packages.
 pip install -r deps/requirements.txt
---
+++ Reddit/src/reddit_analysis/sub_reddit_models.py
@@ -0,0 +1,15 @@
+"""
+LATER
+"""
+from nltk.corpus import stopwords
+import re
+
+
+class SubRedditAnalyzer():
+    """
+    More on this later.
+    """
+
+    def __init__():
+        self.stop_words =
+        self.bag_of_words =
