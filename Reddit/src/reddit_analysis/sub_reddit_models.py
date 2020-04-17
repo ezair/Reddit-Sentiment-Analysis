de20b5b8b57878d3da392edbcfeaa8dc1e2f52bd
zairea200 <zairea200@potsdam.edu>
2020-04-17 14:29:37 -0400

    Testing

--- Reddit/src/reddit_analysis/sub_reddit_models.py
+++ Reddit/src/reddit_analysis/sub_reddit_models.py
@@ -29,17 +29,17 @@ class SubRedditAnalyzer():
 
     def __preprocess_comment_for_analysis(self, comment):
         filtered_tokens_in_comment = []
-        for sentence in comment:
-            # We don't wanna have any punctuation in our comment.
-            tokenizer = RegexpTokenizer(r'\w+')
-            tokenized_sentence = tokenizer.tokenize(sentence)
+        # We don't wanna have any punctuation in our comment.
+        tokenizer = RegexpTokenizer(r'\w+')
+        print(comment)
+        tokenized_comment = tokenizer.tokenize(comment)
 
-            # We do not care about words in our stop list, they just cause issues.
-
-            for token in tokenized_sentence:
-                if token.to_lower() not in self.__stop_words:
-                    filtered_tokens_in_comment.append(token)
-
+        # We do not care about words in our stop list, they just cause issues.
+        for token in tokenized_comment:
+            token = token.lower()
+            if token not in self.__stop_words:
+                filtered_tokens_in_comment.append(token)
+        print(filtered_tokens_in_comment)
         # Now we stem each token in the commen that we have.
         # E.g. Words like 'run' or 'running' just become their base form word 'run'
         stemmer = PorterStemmer()
@@ -47,16 +47,24 @@ class SubRedditAnalyzer():
 
         # Finally, we want to look at each token and get its tagging.
         # E.g. is it a noun, a verb, what is it?
-        preprocessed_tokens = nltk.pos_tag(stemed_tokens)
-        print(preprocessed_tokens)
-        print(type(preprocessed_tokens))
-        return preprocessed_tokens
+        # print(stemed_tokens)
+        return stemed_tokens
+
+    def __train_model(self, list_of_comments_as_strings):
+        all_words = []
+        for comment in list_of_comments_as_strings:
+            print("COMMENT: {}".format(comment))
+            all_words += self.__preprocess_comment_for_analysis(comment)
+        
+        print(all_words)
+        # Freq dist of words.
+        freq_distribution_list_of_all_words = nltk.FreqDist(all_words)
+        print(freq_distribution_list_of_all_words)
 
     def analyze_submission(self, submission_id_as_string, show_statistics=False,
                            show_most_frequent_words=False):
         all_submission_comment_objects = \
             self.__reddit_collection.find({'submission': submission_id_as_string})
-
         # We need to get the actual text from our commend objects, since that is
         # what is going to be analyzed.
         all_comments_on_submission_as_strings = \
@@ -64,17 +72,19 @@ class SubRedditAnalyzer():
 
         # Okay, we have now cleaned up each individual comment in the given list,
         # so they are ready to be analysed.
-        all_processed_comments = []
-
-        self. __preprocess_comment_for_analysis(all_comments_on_submission_as_strings)
+        for comment in all_comments_on_submission_as_strings:
+            self.__preprocess_comment_for_analysis(comment)
+            print(comment)
 
         # Now, let's find out what is "positive" and what is "negative".
         # We can get an overall rating of "goodness" and "badness" based off of this.
-        pos_freq = nltk.FreqDist(all_processed_comments)
-        print(pos_freq.most_common(30))
+        # pos_freq = nltk.FreqDist(all_processed_comments)
 
         if show_statistics:
             pass
+        
+        # Testing here.
+        self.__train_model(all_comments_on_submission_as_strings)
 
     def analyze_whole_subreddit(self, subreddit_name):
         pass
