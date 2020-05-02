83b46f5a032e2613ad4eb3bbd3fe6dccbb571d8b
zairea200 <zairea200@potsdam.edu>
2020-05-02 19:35:50 -0400

    Refactored...a lot, made a seperate file/object for comment processing. Working on adding stochastic feature, so that I can speed up the amount of data that the user may want to go through.

--- Reddit/src/analyze_sub_reddits.py
+++ Reddit/src/analyze_sub_reddits.py
@@ -1,7 +1,7 @@
 """
 later.
 """
-from reddit_analysis.sub_reddit_models import SubRedditAnalyzer
+from reddit_analysis.comment_analysis import SubRedditAnalyzer
 from credentials.mongo_credentials import DB_COLLECTION
 
 
@@ -11,7 +11,8 @@ def main():
     hmft_anylsis_results = analyzer.analyze_subreddit('holdmyfeedingtube', sorting_type="top",
                                                       display_all_submission_results=True,
                                                       display_all_comment_results=True,
-                                                      max_number_of_submissions_to_analyze=4)
+                                                      max_number_of_submissions_to_analyze=0,
+                                                      max_number_of_comments_to_analyze=1)
     for key in hmft_anylsis_results:
         print("\nCleaner print out for a whole subreddit: ")
         print("{}: %{}".format(key, round(hmft_anylsis_results[key] * 100, 2)))
