738f14f7cb71da0eab8d1b0159a362eaef33a0a3
zairea200 <zairea200@potsdam.edu>
2020-05-02 22:07:59 -0400

    Stochastic is now working. All issues to that regard are now working properly. Need to add documentation soon.

--- Reddit/src/analyze_sub_reddits.py
+++ Reddit/src/analyze_sub_reddits.py
@@ -9,11 +9,8 @@ def test_subreddit_call(analyzer):
     hmft_anylsis_results = analyzer.analyze_subreddit('holdmyfeedingtube', sorting_type="top",
                                                       display_all_submission_results=True,
                                                       display_all_comment_results=True,
-                                                      max_number_of_submissions_to_analyze=0,
-                                                      max_number_of_comments_to_analyze=1)
-    for key in hmft_anylsis_results:
-        print("\nCleaner print out for a whole subreddit: ")
-        print("{}: %{}".format(key, round(hmft_anylsis_results[key] * 100, 2)))
+                                                      max_number_of_submissions_to_analyze=1,
+                                                      max_number_of_comments_to_analyze=2)
 
 
 def test_submission_call(analyzer):
@@ -21,18 +18,15 @@ def test_submission_call(analyzer):
     test = analyzer.analyze_submission('ca8q81',
                                        sorting_type='top',
                                        display_all_comment_results=True,
-                                       max_number_of_comments_to_analyze=1)
-    for key in test:
-        print("\nCleaner print out for a whole subreddit: ")
-        print("{}: %{}".format(key, round(test[key] * 100, 2)))
+                                       max_number_of_comments_to_analyze=5)
 
 
 def main():
     # This just a test example program currently.
-    analyzer = SubRedditAnalyzer(DB_COLLECTION)
+    reddit_analyzer = SubRedditAnalyzer(DB_COLLECTION)
 
-    # test_subreddit_call(analyzer)
-    test_submission_call(analyzer)    
+    # test_subreddit_call(reddit_analyzer)
+    test_submission_call(reddit_analyzer)    
 
 if __name__ == '__main__':
     main()
