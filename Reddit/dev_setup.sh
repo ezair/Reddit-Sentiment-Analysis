2f3224ef3df43e0bfc509f40b84394bbd6a3a0b2
zairea200 <zairea200@potsdam.edu>
2020-05-02 20:17:02 -0400

    Updated dev_setup script and moved location of generate_open_docs. Also configured that file to work in different locations.

--- Reddit/dev_setup.sh
+++ Reddit/dev_setup.sh
@@ -1,3 +1,15 @@
+# @author Eric Zair
+# @file dev_setup.sh
+# @date: 05/02/2020
+#
+# Simple setup bash script used to setup this project
+# for users that are running linux.
+#
+# We install all python packages needed, install the resources for
+# NLTK, install doxygen (for project documentation), and finally,
+# run the documentation setup script that I built.
+
+
 # Make sure that python is setup correctly.
 sudo apt-get install python3 python-dev python3-dev \
      build-essential libssl-dev libffi-dev \
@@ -14,3 +26,9 @@ sudo pip install pymongo
 
 # python script that just downloads and installs the nltk library.
 python3 ./deps/nltk_downloader.py
+
+# All of this projects doccumentation is supported by doxygen.
+# We want the user to load in documentation for the project using the
+# generate_open_docs.py script that I wrote.
+sudo apt-get install doxygen
+python3 ./docs/generate_open_docs.py
